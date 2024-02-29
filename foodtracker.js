aad3e4990749c0107d6ea6c92bd81324c93de864
Luca Bisanti <luca_bisanti@sluz.ch>
2024-02-29 14:35:44 +0100

    Foodtracker-API basis

---
+++ foodtracker.js
@@ -0,0 +1,37 @@
+function getNutrition() {
+    var food = document.getElementById("foodInput").value;
+    var apiKey = "ac34c59b2a997870eff4326048397fb0";
+
+    var url = `https://api.edamam.com/api/nutrition-details?app_id=486f122c&app_key=${apiKey}`;
+    var data = {
+        ingr: [food]
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Ergebnisse auf der Seite anzeigen
+        showNutritionInfo(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function showNutritionInfo(data) {
+    const nutritionInfoDiv = document.getElementById('nutritionInfo');
+
+    nutritionInfoDiv.innerHTML = '';
+
+    for (const [nutrient, value] of Object.entries(data.totalNutrients)) {
+        const nutrientItem = document.createElement('p');
+        nutrientItem.textContent = `${value.label}: ${value.quantity} ${value.unit}`;
+        nutritionInfoDiv.appendChild(nutrientItem);
+    }
+}
